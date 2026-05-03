1347d005b248b81dbb631cfea317bf4baef8ad5a
Dhanushvardan <avj25012003@gmail.com>
2026-05-03 13:04:42 +0530

    deploy_ready

--- api/main.py
+++ api/main.py
@@ -9,7 +9,8 @@ from langgraph.graph import  StateGraph
 from langchain_core.documents import Document
 # from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 import os
 from dotenv import load_dotenv
 
@@ -44,7 +45,7 @@ class state(TypedDict):
 text = ""
 inp = ""
 
-embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/paraphrase-MiniLM-L3-v2")
 
 
 def ragNode(state):
